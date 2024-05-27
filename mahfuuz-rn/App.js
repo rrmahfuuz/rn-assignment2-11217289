55501d5d74982258d723a749315114944a1b250a
rrmahfuuz <msmustapha@st.ug.edu.gh>
2024-05-27 16:43:21 +0000

    text bolded

--- mahfuuz-rn/App.js
+++ mahfuuz-rn/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Mahfuuz Mustapha</Text>
+      <Text style={styles.text}>My name is <Text style={styles.bold}>Mahfuuz Mustapha</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +21,8 @@ const styles = StyleSheet.create({
 
   text: {
     fontSize: 24,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });
