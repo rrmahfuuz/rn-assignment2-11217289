cb350922009549da84be2c2c592a16156bb33475
rrmahfuuz <msmustapha@st.ug.edu.gh>
2024-05-27 16:37:09 +0000

    Font size increased to 24

--- mahfuuz-rn/App.js
+++ mahfuuz-rn/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Mahfuuz Mustapha</Text>
+      <Text style={styles.text}>My name is Mahfuuz Mustapha</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#008000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
   },
 });
